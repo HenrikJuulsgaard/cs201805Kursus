effc8f0495520e575516deff0617e90dc69de171
HenrikJuulsgaard <henrikpoulsen_87@hotmail.com>
2018-05-23 15:38:34 +0200

    New Method

---
+++ cd201805 ArraySort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cd201805_ArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] test = { 10, 7, 6, 3, 1, 50, 3 };
+            var res = BeregnOgSorterArray(test);
+
+        }
+
+        static ArrayResultat BeregnOgSorterArray(int[] arr)
+        {
+            ArrayResultat LocArr = new ArrayResultat();
+            LocArr.gns = arr.Average();
+            LocArr.sum = arr.Sum();
+            return LocArr;
+        }
+
+        struct ArrayResultat
+        {
+            public double sum;
+            public double gns;
+        }
+    }
+}
---
+++ cs201805 metoder/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs201805_metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = LægSammen(5, 2);
+            Console.WriteLine(res);
+
+            double res2 = BeregnAreal(5);
+            Console.WriteLine(res2);
+
+            Udskriv();
+            Udskriv("Dette er en test");
+
+            double res3 = BeregnMoms(100.35);
+            Console.WriteLine(res3);
+
+            int[] løn = { 10000, 5000, 30000 };
+            double gns = Gennemsnit(løn);
+            Console.WriteLine(gns);
+
+
+
+            Console.Read();
+        }
+
+        static int LægSammen(int x, int y)
+        {
+            int temp;
+            temp = x + y;
+            return temp;
+        }
+
+        static double BeregnAreal(int radius)
+        {
+            return (radius * radius * System.Math.PI);
+        }
+
+        static void Udskriv(string txt = "")
+        {
+            Console.WriteLine(txt);
+        }
+
+        static double BeregnMoms(double Beløb, double momsPct=0.25)
+        {
+            return Beløb * momsPct;
+        }
+
+        static double Gennemsnit(int[] månedsløn)
+        {
+            return månedsløn.Average();
+        }
+
+    }
+}
